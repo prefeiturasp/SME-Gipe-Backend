31dd31ed824c18ec3b8f7d724d961f79d510c869
MatheusL-Dev <darkaixxt@gmail.com>
2025-07-29 13:54:47 -0300

    feat: :sparkles: Adiciona novos arquivos de serializers para validacao

---
+++ apps/users/api/serializers/validate_login.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+import re
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
+
+    def validate(self, attrs):
+        username = attrs.get("username")
+        digits = re.sub(r'\D', '', username or '')
+
+        if not digits.isdigit() or len(digits) not in (7, 8, 11):
+            raise serializers.ValidationError({
+                'username': "Login inválido. Use RF (7/8 dígitos) ou CPF (11 dígitos numéricos)."
+            })
+
+        attrs['username'] = digits
+        attrs['auth_method'] = 'cpf' if len(digits) == 11 else 'rf'
+        return attrs
