e97f68bdaed5c8b7d6300df47e4bcb3b23feb061
MatheusL-Dev <darkaixxt@gmail.com>
2026-05-07 10:44:58 -0300

    feat: :fire: Remove arquivos de carga unica das unidades no projeto

--- apps/unidades/urls.py
+++ apps/unidades/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from apps.unidades.api.views.carga_unidades_viewset import CargaUnidadeViewSet
 from apps.unidades.api.views.unidades import UnidadeViewSet
 from apps.unidades.api.views.gestao_unidade_viewset import GestaoUnidadeViewSet
 
@@ -9,7 +8,6 @@ app_name = "unidades"
 router = DefaultRouter()
 router.register(r"gestao-unidades", GestaoUnidadeViewSet, basename="gestao-unidades")
 router.register(r'', UnidadeViewSet, basename='unidade')
-router.register(r"carga-de-unidades", CargaUnidadeViewSet, basename="carga-de-unidades")
 
 
 urlpatterns = [
